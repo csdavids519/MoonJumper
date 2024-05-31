290bb76bb0499e9f92f2ed771cb2375b5124633c
csdavids519 <csdavids@gmail.com>
2024-05-31 21:50:05 +0100

    Add modals for game hints popups

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MoonJumper
 Jump around on the moon
+<a href='https://pngtree.com/freepng/computer-keyboard-transparent_8957497.html'>png image from pngtree.com/</a>
--- assets/scripts/indexpage.js
+++ assets/scripts/indexpage.js
@@ -9,6 +9,24 @@ const howToButtonEl = document.getElementById("howToButton");
 const gamePreviewButtonEl = document.getElementById("gamePreviewButton");
 const backButtonEl = document.getElementById("backButton");
 
+const modalFuel = document.querySelector(".gamehint_fuel");
+const modalJump = document.querySelector(".gamehint_jump");
+const modalJetJump = document.querySelector(".gamehint_jetjump");
+const ModalJumpObject = document.querySelector(".gamehint_jumpobject");
+const modalScore = document.querySelector(".gamehint_score");
+
+const openModalFuel = document.querySelector(".info_box_1")
+const openModalJump = document.querySelector(".info_box_2")
+const openModalJetJump = document.querySelector(".info_box_3")
+const openModalJumpObject = document.querySelector(".info_box_4")
+const openModalScore = document.querySelector(".info_box_5")
+
+const closeModalFuel = document.querySelector(".modalclose_fuel")
+const closeModalJump = document.querySelector(".modalclose_jump")
+const closeModalJetJump = document.querySelector(".modalclose_jetjump")
+const closeModalJumpObject = document.querySelector(".modalclose_jumpobject")
+const closeModalScore = document.querySelector(".modalclose_score")
+
 // click how to play button display new text and back button, hide welcome text and how to button
 howToButtonEl.addEventListener('click', () => {
     howToPlayText.classList.add('active');
@@ -34,7 +52,45 @@ backButtonEl.addEventListener('click', () => {
     gamePreview.classList.remove('active');
     welcomeText.classList.add('active');
     howToButton.classList.add('active');
-
-
-
 });
+
+// manage modals
+openModalFuel.addEventListener('click', () => {
+    modalFuel.showModal();
+})
+
+openModalJump.addEventListener('click', () => {
+    modalJump.showModal();
+})
+
+openModalJetJump.addEventListener('click', () => {
+    modalJetJump.showModal();
+})
+
+openModalJumpObject.addEventListener('click', () => {
+    ModalJumpObject.showModal();
+})
+
+openModalScore.addEventListener('click', () => {
+    modalScore.showModal();
+})
+
+closeModalFuel.addEventListener('click', () => {
+    modalFuel.close();
+})
+
+closeModalJump.addEventListener('click', () => {
+    modalJump.close();
+})
+
+closeModalJetJump.addEventListener('click', () => {
+    modalJetJump.close();
+})
+
+closeModalJumpObject.addEventListener('click', () => {
+    ModalJumpObject.close();
+})
+
+closeModalScore.addEventListener('click', () => {
+    modalScore.close();
+})

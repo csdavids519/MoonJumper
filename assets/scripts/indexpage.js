b278cca71f9ec20ba7fe84174f2c2fc419b89213
csdavids519 <csdavids@gmail.com>
2024-06-02 05:13:31 +0100

    Clean up css and html, remove duplicate classes

--- assets/scripts/indexpage.js
+++ assets/scripts/indexpage.js
@@ -1,12 +1,12 @@
 const howToPlayText = document.querySelector(".splash_howtoplay");
 const welcomeText = document.querySelector(".splash_welcome");
-const gamePreview = document.querySelector(".splash_overview");
+const gameControls = document.querySelector(".splash_controls");
 const backButton = document.querySelector(".backButton");
 const howToButton = document.querySelector(".howToButton");
-const previewButton = document.querySelector(".gamePreviewButton");
+const gameControlsButton = document.querySelector(".gameControlsButton");
 
 const howToButtonEl = document.getElementById("howToButton");
-const gamePreviewButtonEl = document.getElementById("gamePreviewButton");
+const gameControlsButtonEl = document.getElementById("gameControlsButton");
 const backButtonEl = document.getElementById("backButton");
 
 const modalFuel = document.querySelector(".gamehint_fuel");
@@ -30,17 +30,17 @@ const closeModalScore = document.querySelector(".modalclose_score")
 // click how to play button display new text and back button, hide welcome text and how to button
 howToButtonEl.addEventListener('click', () => {
     howToPlayText.classList.add('active');
-    previewButton.classList.add('active');
+    gameControlsButton.classList.add('active');
     backButton.classList.add('active');
     welcomeText.classList.remove('active');
     howToButton.classList.remove('active');
 });
 
 // click game preview button 
-gamePreviewButtonEl.addEventListener('click', () => {
+gameControlsButtonEl.addEventListener('click', () => {
     howToPlayText.classList.remove('active');
-    previewButton.classList.remove('active');
-    gamePreview.classList.add('active');
+    gameControlsButton.classList.remove('active');
+    gameControls.classList.add('active');
     backButton.classList.add('active');
 });
 
@@ -48,8 +48,8 @@ gamePreviewButtonEl.addEventListener('click', () => {
 backButtonEl.addEventListener('click', () => {
     howToPlayText.classList.remove('active');
     backButton.classList.remove('active');
-    previewButton.classList.remove('active');
-    gamePreview.classList.remove('active');
+    gameControlsButton.classList.remove('active');
+    gameControls.classList.remove('active');
     welcomeText.classList.add('active');
     howToButton.classList.add('active');
 });
